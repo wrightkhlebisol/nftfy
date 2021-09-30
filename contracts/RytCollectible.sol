74224cffd977cecd5f43e80dce0872b1becc0bdf
Wright <calebogundiya@gmail.com>
2021-09-30 04:33:02 +0100

    Mint and setURI

--- contracts/RytCollectible.sol
+++ contracts/RytCollectible.sol
@@ -1,5 +1,33 @@
 // SPDX-License-Identifier: Unlicensed
 pragma solidity ^0.8.4;
 
-contract RytCollectible is 
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract RytCollectible is ERC721URIStorage{
+
+    string NAME = "Ryt Collectibles";
+    string SYMBOL = "ABS";
+    uint256 private _tokenId = 1;
+
+    // marks an item as available for minting
+    mapping(string => bool) public notMinted;
+
+    constructor(string memory assetForSale) public ERC721(NAME, SYMBOL){
+        notMinted[assetForSale] = true;
+    }
+
+    function mintAndSetURI(string memory tokenURI) external returns(uint){
+        // only "notMinted" items
+        require(notMinted[tokenURI], "Not for sale");
+        notMinted[tokenURI] = false;
+
+        _mint(msg.sender, tokenId);
+
+        // set tokenURI for plugging in our metadata to NFT
+        _setTokenURI(tokenId, tokenURI);
+
+        return _tokenId;
+    }
+
+}
 
